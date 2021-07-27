f051fd1bfcda2d97aad3fbdb36a6d4bbb48bbf72
Sanskar9000 <songara309@gmail.com>
2021-07-28 00:57:21 +0530

    Added index and create pages for index, with methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,26 @@
 class ArticlesController < ApplicationController
+	# shows a single article
 	def show
 	 @article = Article.find(params[:id])
+	end
+
+	def index
+	 @articles = Article.all
+	end
+
+	def new
+		@article = Article.new()
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		# render plain: @article.inspect    #to render any plain text
+ 		if @article.save
+ 		  flash[:notice] = "Article created succesfully"
+ 		  redirect_to @article #article_path(@article)
+
+		else
+			render 'new'
+		end
 	end
 end
