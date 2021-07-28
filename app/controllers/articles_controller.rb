9f6997db5cfaaceef857ea766858feaba9b95c96
Sanskar9000 <songara309@gmail.com>
2021-07-29 00:32:49 +0530

    Added update and delete functionality,used prefix paths

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,19 @@
 class ArticlesController < ApplicationController
 	# shows a single article
 	def show
-	 @article = Article.find(params[:id])
+	 	@article = Article.find(params[:id])
 	end
 
 	def index
-	 @articles = Article.all
+		@articles = Article.all
 	end
 
 	def new
 		@article = Article.new()
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def create
@@ -18,9 +22,25 @@ class ArticlesController < ApplicationController
  		if @article.save
  		  flash[:notice] = "Article created succesfully"
  		  redirect_to @article #article_path(@article)
-
 		else
 			render 'new'
 		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article Updated succesfully"
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy		
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # get 'pages/about'             #this refers to /pages/about
   get 'about', to: 'pages#about'  #this refers to /about
 
-  resources :articles, only: [:show, :index, :new, :create ]
+  resources :articles   #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
